3e36c465e5043b9ea3e62172d7980bf7e85f43d2
cake <c4ke@m.me>
2019-12-02 11:39:08 -0500

    not done but done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import NumberDisplay from "./components/NumberDisplay";
 
 function App() {
+  /*const numberDisplay = numbers.map((number, key) => (
+    <p key={key}>
+      {p.key}
+      </p>*/
+   
   return (
     <div className="App">
-      <Header>
-      This is my awesome header!!
-    </Header>
+      <Header>This is my awesome header!!</Header>
+      <div className="Number">
+        <button onClick={NumberDisplay}>Test</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/NumberDisplay.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function NumberDisplay(props) {
+   return <div className="Number">{props.children}</div>;
+}
+export default NumberDisplay;
